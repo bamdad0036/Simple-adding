4d3fdb1725eead14e75c0a0e9998cbd7494f284e
Bamdad Goshtasbi <Bamdadgoshtasbi@gmail.com>
2021-07-23 01:22:15 -0700

    finially some progress

--- Simple printing.py
+++ Simple printing.py
@@ -1,4 +1,6 @@
 print(2 + 3)
-
+print (print(2), print(3))
 #ok now this is the new code that I impeminiting it in my main and I want to add it again to another
 #file 
+# okkkkk, now this is the main file from git and using vs code which contain and extrea
+#equation
--- simple multiplication.py
+++ simple multiplication.py
@@ -1,3 +1,3 @@
 print(2 * 3)
-
+print( 3 * 3* 4* 5)
 #heeey this text is from the github
